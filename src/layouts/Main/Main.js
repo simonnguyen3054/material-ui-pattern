03f685bc25000e6b0fc8952a6fc53ac5324709bb
singuye <simon_nguyen@gap.com>
2020-01-17 15:07:53 -0800

    initial setup

---
+++ src/layouts/Main/Main.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles, useTheme } from '@material-ui/styles';
+
+const useStyles = makeStyles(theme => ({
+  
+}));
+
+const Main = props => {
+  return (
+   <div>Main Layout</div>
+  );
+};
+
+Main.propTypes = {
+  children: PropTypes.node
+};
+
+export default Main;
---
+++ src/views/Dashboard/Dashboard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles(theme => ({
+ 
+}));
+
+const Dashboard = () => {
+  const classes = useStyles();
+
+  return (
+    <div>Dashboard</div>
+  );
+};
+
+export default Dashboard;
