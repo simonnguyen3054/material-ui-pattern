372b89cf2deed612892c28b3ccc2c1cf3a46d3cc
singuye <simon_nguyen@gap.com>
2020-01-21 15:38:30 -0800

    add icons to topBar

--- src/layouts/Main/components/Topbar/Topbar.js
+++ src/layouts/Main/components/Topbar/Topbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import clsx from "clsx";
 import PropTypes from "prop-types";
 import { Link as RouterLink } from "react-router-dom";
@@ -11,13 +11,21 @@ import InputIcon from "@material-ui/icons/Input";
 const useStyles = makeStyles(theme => ({
   root: {
     boxShadow: "none"
+  },
+  flexGrow: {
+    flexGrow: 1
+  },
+  signOutButton: {
+    marginLeft: theme.spacing(1)
   }
 }));
 
 const Topbar = props => {
-  const { className, ...rest } = props;
+  const { className, onSidebarOpen, ...rest } = props;
 
   const classes = useStyles();
+
+  const [notifications] = useState([]);
 
   return (
     <AppBar {...rest} className={clsx(classes.root, className)}>
@@ -25,6 +33,32 @@ const Topbar = props => {
         <RouterLink to="/">
           <img alt="Logo" src="/images/logos/logo--white.svg" />
         </RouterLink>
+        <div className={classes.flexGrow} />
+        <Hidden mdDown>
+          <IconButton color="inherit">
+            <Badge
+                badgeContent={notifications.length}
+                color="primary"
+                variant="dot"
+            >
+              <NotificationsIcon />
+            </Badge>
+          </IconButton>
+          <IconButton
+              className={classes.signOutButton}
+              color="inherit"
+          >
+            <InputIcon />
+          </IconButton>
+        </Hidden>
+        <Hidden lgUp>
+          <IconButton
+              color="inherit"
+              onClick={onSidebarOpen}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Hidden>
       </Toolbar>
     </AppBar>
   );
